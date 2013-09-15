f24efc45524c533dd10851d26166a2a4f4f86e49
Edmundas Ramanauskas <edmundas.ramanauskas@gmail.com>
2013-09-15 22:37:29 +0300

    router and web.xml samples added

---
+++ src/main/java/RouterExample.java
@@ -0,0 +1,16 @@
+import com.nanomvc.Router;
+import java.util.HashMap;
+import java.util.Map;
+
+public class RouterExample extends Router {
+
+    public Map routes() {
+        Map routes = new HashMap();
+        
+        // controller name without suffix 'Controller'
+        // and action name without prefix 'do'
+        routes.put("/some/path",               "Pages.index");
+        
+        return routes;
+    }
+}
